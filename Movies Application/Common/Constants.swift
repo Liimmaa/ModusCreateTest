4d441684a5ece5a5efb7633855c5fc541b027dc1
liimmaa <halimammegwa@gmail.com>
2022-06-24 18:57:12 +0100

    Setup popular movies UI

--- Movies Application/Common/Constants.swift
+++ Movies Application/Common/Constants.swift
@@ -2,8 +2,11 @@
 //  Copyright © Modus Create. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct Constants {
 	static let apiKey = "bf718d4dd8b23985d9c3edbcfd440a27"
+	static let reuseIdentifier = "Cell"
+	static let title = "Popular"
+	static let spacing: CGFloat = 10
 }
--- Movies Application/Features/Popular Movies/PopularMovieCellContentView.swift
+++ Movies Application/Features/Popular Movies/PopularMovieCellContentView.swift
@@ -9,10 +9,43 @@ extension PopularMovieCell {
 		let summary: MovieSummary
 		
 		var body: some View {
-			ZStack(alignment: .bottom) {
-				Color.black
+			GeometryReader { geo in
+				VStack(spacing: 0) {
+					AsyncImage(url: summary.thumbnailURL, content: {
+						image in
+						image
+							.resizable()
+							.clipShape(Circle())
+							.frame(width: geo.size.width * 0.9, height: geo.size.height * 0.6)
+					}, placeholder: {
+						Color.white
+					})
+					.overlay(
+						summary.isPopular
+						? Circle().stroke(Color.purple, style: StrokeStyle(lineWidth: 5, dash: [15]))
+						: nil
+					)
+					
+					Spacer()
+					
+					VStack(alignment: .leading, spacing: 4) {
+						Text(summary.title)
+							.padding(4)
+							.fixedSize(horizontal: false, vertical: true)
+							.font(.headline)
+							.background(Color.gray.opacity(0.2))
+							.overlay(RoundedRectangle(cornerRadius: 6).stroke(Color.yellow, lineWidth: 1))
+							.multilineTextAlignment(.leading)
+						
+						Text(summary.overview)
+							.font(.subheadline)
+							.lineLimit(3)
+						
+					}
+					.padding(.horizontal, 6)
+					.frame(width: geo.size.width, height: geo.size.height * 0.4)
+				}
 			}
-			.ignoresSafeArea()
 		}
 	}
 }
